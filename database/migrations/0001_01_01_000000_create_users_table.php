2e3f09b729148a90a1e9ff1b1951ee498332ce32
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2025-01-28 12:01:28 +0700

    migration: modify users table for implement google authentication

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,11 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('google_id')->nullable();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('avatar_url');
+            $table->string('instansi');
             $table->timestamps();
         });
 
