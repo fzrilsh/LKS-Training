556642e0a21dc00f251c59c59907b2609ba3945c
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2025-01-29 19:03:57 +0700

    migration: add table for save uploaded assets by user

---
+++ app/Models/UploadedAsset.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class UploadedAsset extends Model
+{
+    public $timestamps = false;
+
+    protected $fillable = [
+        'user_id',
+        'path'
+    ];
+
+    public function User(): BelongsTo{
+        return $this->belongsTo(User::class);
+    }
+}
