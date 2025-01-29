e572c478303deb20790be1e5fb7c035b5b7dbce2
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2025-01-29 12:30:03 +0700

    feat: add new routings

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,20 @@
 <?php
 
 use App\Http\Controllers\Auth\SocialiteController;
+use App\Livewire\Auth\Login;
+use App\Livewire\ClientPage\Dashboard;
+use App\Livewire\LandingPage;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', LandingPage::class)->name('dashboard');
+
 Route::middleware(['auth:web'])->group(function(){
-    Route::get('/', function () {
-        return view('welcome');
-    })->name('dashboard');
+    Route::group(['prefix' => 'clientarea', 'as' => 'clientarea'], function(){
+        Route::get('/', Dashboard::class)->name('.dashboard');
+    });
 });
+
+Route::get('/login', Login::class)->name('login');
 
 Route::controller(SocialiteController::class)->group(function(){
     Route::get('/auth/google', 'googleRedirect')->name('auth.google');
