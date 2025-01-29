dae1c131461e4a0a19c2c9dcb79e7d606dca0612
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2025-01-29 19:19:16 +0700

    feat: create route for upload assets, logout, and add middleware for route login

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,28 @@
 <?php
 
 use App\Http\Controllers\Auth\SocialiteController;
+use App\Http\Middleware\LoggedInMiddleware;
 use App\Livewire\Auth\Login;
 use App\Livewire\ClientPage\Dashboard;
+use App\Livewire\ClientPage\UploadAssets;
 use App\Livewire\LandingPage;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', LandingPage::class)->name('dashboard');
+Route::middleware([LoggedInMiddleware::class])->get('/login', Login::class)->name('login');
 
 Route::middleware(['auth:web'])->group(function(){
+    Route::get('/logout', function(){
+        auth('web')->logout();
+        return redirect()->route('login');
+    })->name('logout');
+
     Route::group(['prefix' => 'clientarea', 'as' => 'clientarea'], function(){
         Route::get('/', Dashboard::class)->name('.dashboard');
+        Route::get('/upload-assets', UploadAssets::class)->name('.upload-assets');
     });
 });
 
-Route::get('/login', Login::class)->name('login');
 
 Route::controller(SocialiteController::class)->group(function(){
     Route::get('/auth/google', 'googleRedirect')->name('auth.google');
