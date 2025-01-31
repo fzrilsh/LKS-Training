3a7576ece5f2bafa9074f7cb95f5f3a6fe5aee19
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2025-01-31 08:40:28 +0700

    feat: add handler for not found exception

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,5 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function(NotFoundHttpException $e){
+            return response()->view('livewire.conditions.not-found', [], 404);
+        });
     })->create();
