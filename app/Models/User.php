b63feb4ad0ae8cf8d6f11aee849214327f8e4527
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2025-01-31 08:41:15 +0700

    feat: create has many relation to uploadedassets

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
@@ -42,6 +43,10 @@ class User extends Authenticatable
 
     public function ModuleToken(): HasOne {
         return $this->hasOne(ModuleToken::class, 'user_id', 'id');
+    }
+
+    public function uploadedAssets(): HasMany {
+        return $this->hasMany(UploadedAsset::class);
     }
 
     public function getModuleTasksAttribute(){
