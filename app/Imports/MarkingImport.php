3246a4f80f5f0ca6c7eb9b58b9258ec0bbec9129
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2025-02-01 23:37:24 +0700

    refactor: remove all method on this subhanallah file

--- app/Imports/MarkingImport.php
+++ app/Imports/MarkingImport.php
@@ -8,18 +8,6 @@ class MarkingImport implements ToArray
 {
     public function array(array $rows)
     {
-        $filtered = collect($rows)->filter(function ($row) {
-            return isset($row[3]) && $row[3] === 'M';
-        });
-
-        $results = $filtered->map(function ($row) {
-            return [
-                'description' => $row[4] ?? null,
-                'total_point' => $row[10] ?? null,
-                'point' => 0,
-            ];
-        });
-
-        return $results->toArray();
+        return $rows;
     }
 }
